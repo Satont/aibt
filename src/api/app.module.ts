97ba7d4550f1c83d33eb8b1098f29db6f47a8453
Satont <satontworldwide@gmail.com>
2021-06-26 02:55:16 +0300

    upd

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { NotableplayersModule } from './notableplayers/notableplayers.module';
-import { MedalsController } from './medals/medals.controller';
 import { MedalsModule } from './medals/medals.module';
 
 @Module({
